fdcb4dc055b13566a2edf7b23242c84c3c8838df
vuongductien <tienducvuong86@gmail.com>
2025-12-30 20:20:55 +0700

    30-12-2025

---
+++ 2025-12-27/main.js
@@ -0,0 +1,40 @@
+// console.log("Hello World")
+// console.log("Hello","45")
+//
+// let a = 1
+// a=4
+// a=2
+// console.log(a)
+
+//
+// const Name="Jerry"
+// console.log(typeof Name)
+//
+// console.log(typeof(123))
+
+// let a = 123
+// console.log(typeof a)
+//
+// a = String(a)
+// console.log(typeof a)
+//
+
+
+const numbers = [1,2,3]    // mang 1 chieu
+console.log(numbers)
+console.log(numbers[0])  // lay gia tri cua mot phan tu trong mang
+
+const names = ["Jerry","Tom","Jack"]
+console.log(names)
+
+
+const Array  = [1, [1,3,4] , 4]
+console.log(Array)
+
+
+const dict= {
+    name:"Jerry",
+    age:18
+}
+console.log(dict)
+
